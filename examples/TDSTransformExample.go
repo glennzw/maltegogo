ca4722a478492943b578f480b35ac42b98bf85be
glenn <glenn@sensepost.com>
2016-02-24 16:52:12 +0000

    Updated example

--- examples/TDSTransformExample.go
+++ examples/TDSTransformExample.go
@@ -14,28 +14,50 @@ func defaultResponse(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "No")
 }
 
-func dnsReverse(w http.ResponseWriter, r *http.Request) {
+func parseXML(r *http.Request) string {
 	r.ParseForm()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		panic(err)
 	}
 	if len(body) > 0 {
-		data := string(body)
-		m := maltegogo.MaltegoMsg(data)
+		return string(body)
+	}
+	return ""
+}
 
+func dnsForward(w http.ResponseWriter, r *http.Request) {
+	data := parseXML(r)
+	if data != "" {
+		m := maltegogo.MaltegoMsg(data)
+		hostname := m.Value
+		revHosts, _ := net.LookupHost(hostname)
+		TRX := maltegogo.MaltegoTransform{}
+		for _, ip := range revHosts {
+			fmt.Println("Forward resolved " + hostname + " to " + ip)
+			TRX.AddEntity("maltego.IPv4Address", ip)
+		}
+		fmt.Fprintf(w, TRX.ReturnOutput()) // send data to client side
+	} else {
+		fmt.Fprintf(w, "Error: No form data")
+	}
+}
+
+func dnsReverse(w http.ResponseWriter, r *http.Request) {
+	data := parseXML(r)
+	if data != "" {
+		m := maltegogo.MaltegoMsg(data)
 		ip := m.Value
 		revIPs, _ := net.LookupAddr(ip)
 		TRX := maltegogo.MaltegoTransform{}
 		for _, host := range revIPs {
-			fmt.Println(host)
+			fmt.Println("Reverse resolved " + ip + " to " + host)
 			NewEnt := TRX.AddEntity("maltego.DNSName", host)
 			NewEnt.AddProperty("hostname", "Hostname", "stict", host)
 		}
-
 		fmt.Fprintf(w, TRX.ReturnOutput()) // send data to client side
 	} else {
-		fmt.Fprintf(w, "No form data")
+		fmt.Fprintf(w, "Error: No form data")
 	}
 }
 
@@ -43,6 +65,7 @@ func main() {
 	http.HandleFunc("/", defaultResponse)
 	//Add Transform endpoints here (add to TDS)
 	http.HandleFunc("/reverseDNS", dnsReverse)
+	http.HandleFunc("/forwardDNS", dnsForward)
 
 	err := http.ListenAndServe(":9090", nil) // set listen port
 	if err != nil {
